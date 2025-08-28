387318631648bf050f9c5edd98ceca89feeca347
maggot-code <udp.pkg.loss@gmail.com>
2025-08-29 02:20:53 +0800

    为RemoteCallSerializer添加详细的错误消息，增强字段验证的用户反馈

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,35 @@
 from rest_framework import serializers
 
 class RemoteCallSerializer(serializers.Serializer):
-    os_type = serializers.ChoiceField(choices=["linux", "windows"])
-    ip = serializers.IPAddressField()
-    username = serializers.CharField()
-    password = serializers.CharField(required=False, allow_blank=True)
-    port = serializers.IntegerField(required=False, min_value=1, max_value=65535)
-    command = serializers.CharField(required=False, allow_blank=True)
-    file_path = serializers.CharField(required=False, allow_blank=True)
+    os_type = serializers.ChoiceField(
+        choices=["linux", "windows"],
+        error_messages={
+            "required": "OS type is required",
+            "invalid_choice": "OS type must be one of [linux | windows]"
+        }
+    )
+    ip = serializers.IPAddressField(
+        error_messages={
+            "required": "IP address is required",
+            "invalid": "Please enter a valid IP address"
+        }
+    )
+    username = serializers.CharField(
+        error_messages={
+            "required": "Username is required",
+            "blank": "Username cannot be blank"
+        }
+    )
+    password = serializers.CharField(required=False,allow_blank=True)
+    port = serializers.IntegerField(
+        required=False,
+        min_value=1,
+        max_value=65535,
+        error_messages={
+            "invalid": "Port must be an integer",
+            "min_value": "Port cannot be less than 1",
+            "max_value": "Port cannot be greater than 65535"
+        }
+    )
+    command = serializers.CharField(required=False,allow_blank=True)
+    file_path = serializers.CharField(required=False,allow_blank=True)
